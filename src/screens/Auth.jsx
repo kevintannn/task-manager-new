c3cd6ff31b8833f40c3f68d27b3e1a5a89d6b6a3
Kevin Tan <48116781+kevintannn@users.noreply.github.com>
2024-05-22 14:01:10 +0700

    change color

--- src/screens/Auth.jsx
+++ src/screens/Auth.jsx
@@ -124,13 +124,13 @@ const Auth = () => {
           {passwordError && <p className=" text-red-600">{passwordError}</p>}
         </div>
 
-        <Link className="text-right  font-bold text-blue-700 hover:underline">
+        <Link className="text-right  font-bold hover:underline">
           Forgot Password?
         </Link>
 
         <button
           ref={loginRef}
-          className="my-5 rounded-md bg-blue-700 p-3  text-white"
+          className="my-5 rounded-md p-3  text-white"
           onClick={handleLogin}
         >
           Login
@@ -140,7 +140,7 @@ const Auth = () => {
           Not registered yet?{" "}
           <Link
             to="/register"
-            className="font-bold text-blue-700 hover:underline"
+            className="font-bold hover:underline"
           >
             Create an account <ArrowOutwardIcon fontSize="small" />
           </Link>
